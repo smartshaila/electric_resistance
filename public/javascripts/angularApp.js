5d621e7d741faf19d5ea44d9f2e7018061333022
smartshaila <smartshaila@gmail.com>
2015-10-23 17:38:51 -0400

    Testing angular sample data

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -3,7 +3,7 @@
  */
 
 angular
-    .module('electric_resistance')
+    .module('electric_resistance', [])
     .controller('GameController',
         function ($scope) {
             $scope.players = [
