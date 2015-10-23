9cf45b245ebd03d20164883db3491bf34d29eb3b
Nicholas <i.am@aweso.ms>
2015-10-23 17:39:58 -0400

    Placeholder Data Commit

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,3 +1,21 @@
 /**
  * Created by shaila on 10/23/15.
  */
+
+angular
+    .module('electric_resistance')
+    .controller('GameController',
+        function ($scope) {
+            $scope.players = [
+                {'name': 'Nicholas', 'logged_in': true},
+                {'name': 'Shaila', 'logged_in': false},
+                {'name': 'Krista', 'logged_in': true},
+                {'name': 'Hitanshu', 'logged_in': true},
+                {'name': 'Sneha', 'logged_in': false}
+            ];
+            $scope.roles = ['Merlin', 'Assassin', 'Percival', 'Morgana', 'Loyal Servant of Arthur'];
+            $scope.waiting_on = ['Shaila'];
+            $scope.waiting_for = 'log in';
+            $scope.mission_number = 3;
+        }
+    );
